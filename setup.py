94e306cbe61603347bd8bb711aa86119bed4f12b
81NewArk <751247667@qq.com>
2025-07-30 09:57:28 +0800

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="AntiCAP",
-    version="4.0.1",
+    version="3.1.4",
     author="NewArk81",
     description="AntiCAP",
     long_description=long_description,
